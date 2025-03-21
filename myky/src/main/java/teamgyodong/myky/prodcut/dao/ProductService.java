039f546111bf7ed77bb6389800bc5dcfdb4592d5
latte28 <qkrwodnjs7953@naver.com>
2025-03-21 16:10:41 +0900

    2025.03.21 중간 커밋

--- myky/src/main/java/teamgyodong/myky/prodcut/dao/ProductService.java
+++ myky/src/main/java/teamgyodong/myky/prodcut/dao/ProductService.java
@@ -1,5 +1,10 @@
 package teamgyodong.myky.prodcut.dao;
 
+import java.util.HashMap;
+
 public interface ProductService {
-//.....
+
+	HashMap<String, Object> getProductList(HashMap<String, Object> map);
+	
+	
 }
