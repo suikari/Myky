b67c2ecdd8ebf78514cb7a881613ec7c5dc344c8
suikari <suikari@naver.com>
2025-03-25 20:37:39 +0900

    25-03-25 관리자 페이지 사이드 분리, 체인지 작업준비

--- myky/src/main/java/teamgyodong/myky/pay/dao/PayServiceImpl.java
+++ myky/src/main/java/teamgyodong/myky/pay/dao/PayServiceImpl.java
@@ -1,11 +1,14 @@
 package teamgyodong.myky.pay.dao;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import teamgyodong.myky.pay.mapper.PayMapper;
+import teamgyodong.myky.pay.model.pay;
+import teamgyodong.myky.product.model.Product;
 
 
 @Service
@@ -21,6 +24,22 @@ public class PayServiceImpl implements PayService {
 		try {
 			payMapper.insertPayment(map);
 
+			resultMap.put("result", "success");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			resultMap.put("result", "fail");			
+		}
+		return resultMap;
+	}
+	
+	@Override
+	public HashMap<String, Object> selectPayment(HashMap<String, Object> map) {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		
+		try {
+			List<pay> pay =  payMapper.selectPayment(map);
+			
+			resultMap.put("pay", pay);
 			resultMap.put("result", "success");
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
