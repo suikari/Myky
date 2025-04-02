3965922f4c2de2d1bc467bfd0c89400a63a5255e
PRPRSPR <s.subin.0208@gmail.com>
2025-04-02 20:17:09 +0900

    250402 - 주문/배송조회 페이지 주문취소,개별상품재구매버튼 구현

--- myky/src/main/java/teamgyodong/myky/cart/dao/CartServiceImpl.java
+++ myky/src/main/java/teamgyodong/myky/cart/dao/CartServiceImpl.java
@@ -256,4 +256,19 @@ public class CartServiceImpl implements CartService {
 		}
 		return resultMap;
 	}
+	
+	@Override
+	public HashMap<String, Object> editOrderStatus(HashMap<String, Object> map) {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		
+		try {
+			cartMapper.updateOrderStatus(map);
+
+			resultMap.put("result", "success");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			resultMap.put("result", "fail");			
+		}
+		return resultMap;
+	}
 }
