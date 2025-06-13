655df01552dde34b900b879664f609890be1ada9
suikari <suikari@naver.com>
2025-06-13 16:18:14 +0900

    25-06-13 프로퍼티 관련 삭제 커밋

--- myky/src/main/java/teamgyodong/myky/MykyApplication.java
+++ myky/src/main/java/teamgyodong/myky/MykyApplication.java
@@ -2,10 +2,18 @@ package teamgyodong.myky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MykyApplication {
+public class MykyApplication extends SpringBootServletInitializer {
 
+    // 추가 코드
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MykyApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MykyApplication.class, args);
 	}
